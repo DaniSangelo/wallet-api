4162a420b46087843c3d210f443ee5dccdd06715
Daniel Sângelo <danielsangelo@hotmail.com>
2025-12-08 14:10:21 -0300

    feat: add column on wallets table

--- database/migrations/2025_12_08_165904_create_wallets_table.php
+++ database/migrations/2025_12_08_165904_create_wallets_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('wallets', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
+            $table->string('account')->unique();
             $table->decimal('balance', 15, 2)->default(0);
             $table->softDeletes();
             $table->timestamps();
